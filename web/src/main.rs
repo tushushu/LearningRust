946ca325daf27601429ab76203f90682571b3890
tushushu <xmaseve1984@gmail.com>
2024-03-03 20:22:05 +0800

    LR

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,28 +1,40 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+use std::ops::{AddAssign, Mul};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
+#[post("/predict")]
+async fn predict(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[derive(Deserialize)]
+struct LinearRegression<T> {
+    coef: Vec<T>,
+    intercept: T,
+}
+
+impl<T> LinearRegression<T>
+where
+    T: Mul<Output = T> + AddAssign + Copy,
+{
+    fn predict(&self, x: Vec<T>) -> T {
+        let mut result = self.intercept;
+        for (i, &coef) in self.coef.iter().enumerate() {
+            result += coef * x[i];
+        }
+        result
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello).service(predict))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
